2f1277e51518139460bd83db9f151c57bdcc9dfa
新井進鎬 <chino19810618@gmail.com>
2016-04-15 17:01:37 +0900

    add initializer in CoreDataStack class.

--- BookList/CoreDataStack.swift
+++ BookList/CoreDataStack.swift
@@ -12,12 +12,30 @@ import CoreData
 class CoreDataStack {
     let context: NSManagedObjectContext
     
+    //永続ファイルのURL
     let appDocumentDirectryURL: NSURL = {
         let fileManager = NSFileManager.defaultManager()
         let urls = fileManager.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask)
         return urls.last!
     }()
     
-    
+    init() {
+        //モデルのURL
+        let bundle = NSBundle.mainBundle()
+        guard let model_URL = bundle.URLForResource("BookListModel", withExtension: "momd") else { fatalError() }
+        
+        //管理オブジェクトモデルを生成
+        guard let model = NSManagedObjectModel(contentsOfURL: model_URL) else { fatalError() }
+        
+        //コーディネータを生成
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        
+        //コンテキストを生成
+        self.context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+
+        //コーディネータと接続
+        context.persistentStoreCoordinator = coordinator
+        
+    }
     
 }
